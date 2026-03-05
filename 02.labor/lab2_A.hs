7cd982ebb621c49bd79a5d3255c1a918ac0d7603
Voliiiiiiii <Botondvoli@gmail.com>
2026-03-05 12:33:23 +0200

    lab3

--- 02.labor/lab2_A.hs
+++ 02.labor/lab2_A.hs
@@ -15,15 +15,55 @@ szjSzorzat3 n res
   | otherwise = szjSzorzat3 (div n 10) (res * (mod n 10))
 
 -- - egy szám számjegyeinek összegét (2 módszerrel),
+szjOsszeg::Integral t => t -> t
+szjOsszeg n
+  | n < 0 = szjOsszeg (abs n)
+  | n < 10 = n
+  | otherwise = mod n 10 + szjOsszeg (div n 10)
+
+
 -- - egy szám számjegyeinek számát (2 módszerrel),
+szjSzam::(Num t, Integral a) => a -> t -> t
+szjSzam n res
+  | n < 0 = szjSzam (abs n) res
+  | n < 10 = res + 1
+  | otherwise = szjSzam (div n 10) (res + 1)
+
+
+
 -- - egy szám azon számjegyeinek összegét, mely paraméterként van megadva, pl. legyen a függvény neve fugv4, ekkor a következő meghívásra, a következő eredményt kell kapjuk:
+szjSzamOsszeg n szj
+  | szj > 9 = error "nem szamj."
+  | n < 10 = if n == szj then szj else 0
+  | otherwise = if mod n 10 == szj then szj + szjSzamOsszeg (div n 10) szj else szjSzamOsszeg(div n 10) szj
+
+
 
 --   ```haskell
 --   > fugv4 577723707 7
 --   35
 --   ```
 -- - egy szám páros számjegyeinek számát,
+parosSzamSzj2 n res
+  | n < 0 = parosSzamSzj2 (abs n) res
+  | n < 10 = if even n then res +1 else res
+  | otherwise =
+      if even (mod n 10)
+        then parosSzamSzj2 (div n 10) (res +1)
+        else parosSzamSzj2 (div n 10) res 
+
+
 -- - egy szám legnagyobb számjegyét,
+
+lgSzj n ln
+  | n < 0 = lgSzj(abs n) ln
+  | n < 10 = if n > ln then n else ln
+  | otherwise = 
+    if mod n 10 > ln
+      then lgSzj(div n 10) (mod n 10)
+      else lgSzj(div n 10) ln
+
+      
 -- - egy szám $b$ számrendszerbeli alakjában a $d$-vel egyenlő számjegyek számát (például a $b = 10$-es számrendszerben a $d = 2$-es számjegyek száma),
 --   Példák függvényhívásokra:
 
